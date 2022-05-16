deb69c93e559f72cf58f40bcecaaf6861c854c32
Alibek <alibek.gao@gmail.com>
2022-05-16 19:31:48 +0200

    init: flow chart

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import Flow from './Flow';
+import { useState } from 'react';
+
+export default () => {
+  const [initial, setInitial] = useState(null)
+  const handleFileSelected = (e) => {
+    const files = Array.from(e.target.files)
+    if (files[0].type !== 'application/json') {
+      alert('Please upload valid JSON file');
+      return;
+    }
+    let reader = new FileReader();
+    reader.onload = (e) => {
+      let str = e.target.result;
+      let json = JSON.parse(str);
+      setInitial(json);
+    };
+    reader.readAsText(files[0]);
+  }
+
+
+  return (
+    <>
+      {initial && (
+        <header className="header">
+          <button onClick={() => setInitial(null)}>back to main menu</button>
+        </header>
+      )}
+      {!initial && (
+        <div>
+          <h1>upload JSON or create new</h1>
+          <input type="file" onChange={handleFileSelected}/>
+          <button onClick={() => setInitial({nodes: [], edges: []})}>Create new chart</button>
+        </div>
+      )}
+      {initial && <Flow initial={initial}/>}
+    </>
+  )
+}
